aec35e192cd75d4903c64756da70b88dbb3269dc
Gulnara <kagulnara@yahoo.com>
2020-04-17 19:51:07 -0700

    lesson_1

--- lesson_1/homework_1_3.py
+++ lesson_1/homework_1_3.py
@@ -1,15 +1,17 @@
 import math
 
 
+
 # Round a off to three decimal places.
 
 a = 10.04
-result_1 = None
+result_1 = round(a, 3)
 
 
 # Round up result_2 to greater value.
 
 result_2 = 5 / 2 * 6 + 1.25 - 4
+
 
 
 # Round up result_3 to lesser value.
